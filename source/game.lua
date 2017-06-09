2bb1c3ac37ac04ef201d7610174675f949feb95d
Jeremy Clarke <geckojsc@gmail.com>
2017-06-09 15:27:46 +0100

    conf, camera and stuff

--- source/game.lua
+++ source/game.lua
@@ -1,8 +1,9 @@
+local Camera = require "external.camera"
 
 local game = {}
 
 function game.load()
-	
+	game.cam = Camera.new(256, 128, 2)
 end
 
 function game.update(dt)
@@ -10,7 +11,9 @@ function game.update(dt)
 end
 
 function game.draw()
-	
+	game.cam:attach()
+	lg.print("hello world!", 10, 10)
+	game.cam:detach()
 end
 
 return game
